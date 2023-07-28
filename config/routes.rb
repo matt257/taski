479672e0da29a5a0e4fce8a0e76c5d8aecd0c286
matt <mattwalton257@gmail.com>
2023-07-27 21:47:23 -0400

    modifed routes, worked on views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'tasks#home'
-  get 'tube-view', to: 'tubes#index'
-  get 'task-index', to: 'tasks#index'
-  get 'status', to: 'status#index'
+  root 'tasks#home' #homepage; (logged in? status#index : tasks#home)
+  get 'tube-view', to: 'tubes#index' #see tasks in tubes
+  get 'task-index', to: 'tasks#index' #see a subset of tasks; referenced by parameters
+  get 'status', to: 'status#index' #start and stop work by tube, view updates; via websocket
   resources :tasks
   resources :tubes
   devise_for :users, controllers: { registrations: 'registrations' }
