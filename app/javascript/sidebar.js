823e2d9a5c795d0a1d1ae421b58212c9acb833ec
matt <matt@ubuntu-1.myguest.virtualbox.org>
2023-07-31 17:14:31 -0400

    working on layouts, updating js

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,3 +1,11 @@
+function toggleSidebar() {
+  const content = document.querySelector('.main-content');
+  document.querySelector('.sidebar').classList.toggle('active');
+  if (content) {
+    content.classList.toggle('active');
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('toggleButton');
   if (button) {
@@ -6,12 +14,4 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("where'd you put the button?");
   }
 });
-
-function toggleSidebar() {
-  const content = document.querySelector('.main-content');
-  document.querySelector('.sidebar').classList.toggle('active');
-  if (content) {
-    content.classList.toggle('active');
-  }
-}
 
