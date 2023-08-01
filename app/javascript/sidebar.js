18066484dc412392688bb180c798d69536edd68a
matt <mattwalton257@gmail.com>
2023-08-01 14:00:44 -0400

    worked on views logic, js

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,8 +1,14 @@
 function toggleSidebar() {
   const content = document.querySelector('.main-content');
+  const login_button = document.getElementById('login-button');
+  const signup_button = document.getElementById('logout-button');
+  
   document.querySelector('.sidebar').classList.toggle('active');
+  
   if (content) {
     content.classList.toggle('active');
+    login_button.classList.toggle('active');
+    signup_button.classList.toggle('active');
   }
 }
 
@@ -10,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('toggleButton');
   if (button) {
     button.addEventListener('click', toggleSidebar);
+    
   } else {
     console.log("where'd you put the button?");
   }
