08121d1553d2f71824878d212d4ce8486bbff738
matt <mattwalton257@gmail.com>
2023-08-02 15:14:11 -0400

    working on Task views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,31 @@
+# tasks controller 
 class TasksController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:home]
-    
-    def home
+  skip_before_action :authenticate_user!
+
+  def index
+    @tasks = current_user.tasks
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def new
+    @task = current_user.tasks.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render :new
     end
+  end
 
-    def index
-        @tasks = Task.all
-    end
+  private
 
-    def show
-        @task = Task.find(params[:id])
-    end
-
-    private
-
-        def task_params
-            params.require(:task).permit(:name, :description, :priority, :tube_number)
-        end
+  def task_params
+    params.require(:task).permit(:name, :description, :priority, :tube_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root 'tasks#home' #homepage; (logged in? status#index : tasks#home)
-  get 'tube-view', to: 'tubes#index' #see tasks in tubes
-  get 'task-index', to: 'tasks#index' #see a subset of tasks; referenced by parameters
-  get 'status', to: 'status#index' #start and stop work by tube, view updates; via websocket
+  root 'tasks#index'
   mount ActionCable.server => '/cable'
   resources :tasks
   resources :tubes
